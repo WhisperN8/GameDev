46bca69510c32fededb598f8cb800109ac030657
WhisperN8 <t.suleymanov@innopolis.university>
2023-06-17 10:43:24 +0300

    OutOfRange CGGD

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -5,17 +5,35 @@
 
 void cg::renderer::rasterization_renderer::init()
 {
-	// TODO Lab: 1.02 Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
-	// TODO Lab: 1.04 Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
-	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
+	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
+	rasterizer->set_viewport(settings->width, settings->height);
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
+			settings->width, settings->height);
+	rasterizer->set_render_target(render_target);
+
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
 }
 void cg::renderer::rasterization_renderer::render()
 {
-	// TODO Lab: 1.02 Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
-	// TODO Lab: 1.04 Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
-	// TODO Lab: 1.05 Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
-	// TODO Lab: 1.03 Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	auto start = std::chrono::high_resolution_clock::now();
+	rasterizer->clear_render_target({255, 255, 255});
+
+	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size(); ++shape_id)
+	{
+		rasterizer->set_vertex_buffer(
+				model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(
+				model->get_index_buffers()[shape_id]);
+		rasterizer->draw(
+				model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
+	}
+
+	auto stop = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float, std::milli> duration = stop - start;
+	std::cout << "Rasterization took " << duration.count() << " ms\n";
+
+	cg::utils::save_resource(*render_target, settings->result_path);
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}
